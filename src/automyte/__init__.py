58d72084594b07db8a381e57a579bda6538862c1
Artur Shyshko <arturshyshko@gmail.com>
2025-03-05 17:21:30 +1100

    Add util vcs tasks for performing vcs cli commands from automaton
    Add basic 'add', 'commit', 'push' vcs tasks to be calling inside
    Automaton postprocess;
    
    They are using VCS.run method and support adding flags to the command,
    via 'flags(*)' method;
    to be used as "from automyte.tasks import vcs; vcs.add(...).flags(...)"
    
    Covered with tests + fix legacy test_vcs scenario.

--- src/automyte/__init__.py
+++ src/automyte/__init__.py
@@ -4,6 +4,7 @@ from automyte.contrib.tasks import *
 from automyte.discovery import *
 from automyte.history import *
 from automyte.project import *
+from automyte.tasks import *
 from automyte.vcs import *
 
 # TODO: Maybe define actual exports in __all__ ?
--- tests/scenarios/test_vcs.py
+++ tests/scenarios/test_vcs.py
@@ -14,6 +14,7 @@ from automyte import (
     TasksFlow,
     guards,
 )
+from automyte.tasks import vcs
 from automyte.utils import bash
 
 
@@ -21,57 +22,6 @@ def lol(ctx: RunContext, file: File):
     import re
 
     file.edit(re.sub(r"world", "there", file.get_contents()))
-
-
-# TODO: This is to be replaced by module, just trialing how it will be called.
-class vcs:
-    class add:
-        def __init__(self, project_root_relative_path: str):
-            self.path = project_root_relative_path
-
-        def __call__(self, ctx: RunContext, file):
-            ctx.vcs.add(path=self.path)
-
-    class commit:
-        def __init__(self, msg: str):
-            self.commit_message = msg
-
-        def __call__(self, ctx: RunContext, file):
-            ctx.vcs.commit(self.commit_message)
-
-    class run:
-        def __init__(self, cmd: str):
-            self.cmd = cmd
-
-        def __call__(self, ctx: RunContext, file):
-            ctx.vcs.run(self.cmd)
-
-
-def test_files_added(tmp_local_project_factory):
-    dir = tmp_local_project_factory(structure={"src": {"hello.txt": "hello world!"}})
-
-    Automaton(
-        name="impl1",
-        config=Config.get_default().set_vcs(dont_disrupt_prior_state=False),
-        projects=[
-            Project(
-                project_id="test_project",
-                explorer=LocalFilesExplorer(rootdir=dir, filter_by=ContainsFilter(contains="hello world")),
-            ),
-        ],
-        flow=TasksFlow(
-            [
-                lol,
-            ],
-            preprocess=[vcs.run("init")],
-            postprocess=[
-                vcs.add("src/"),
-                # vcs.commit("whaaaatt"),
-            ],
-        ),
-    ).run()
-
-    assert "src/hello.txt" in bash.execute(["git", "-C", dir, "status"]).output
 
 
 def test_worktree_setup(tmp_local_project_factory):
