b7eccb371255fda462c3a590f61ce649ce17d119
Artur Shyshko <arturshyshko@gmail.com>
2025-03-06 14:33:51 +1100

    Cover Project() with tests
    Just to make sure core behaviour will not be broken in the future.

--- src/automyte/project/project.py
+++ src/automyte/project/project.py
@@ -1,4 +1,5 @@
 import contextlib
+import typing as t
 
 from automyte.config import Config
 from automyte.discovery import LocalFilesExplorer, ProjectExplorer
@@ -13,9 +14,11 @@ class Project:
         explorer: ProjectExplorer | None = None,
         vcs: VCS | None = None,
     ):
-        assert rootdir or explorer, "Need to supply at least one of: rootdir | explorer."
+        if not rootdir and not explorer:
+            raise ValueError("Need to supply at least one of: rootdir | explorer")
+
         self.project_id = project_id
-        self.explorer = explorer or LocalFilesExplorer(rootdir=rootdir)  # TODO: Fix linter??
+        self.explorer = explorer or LocalFilesExplorer(rootdir=t.cast(str, rootdir))
         self.rootdir = rootdir or self.explorer.get_rootdir()
         self.vcs = vcs or Git(rootdir=self.rootdir)
 
@@ -23,7 +26,7 @@ class Project:
     def in_working_state(self, config: Config):
         """Hook for doing any initial project setup and cleanup after the work is done.
 
-        Real use case for now - adjusting rootdir to point to worktree one if dont_disrupt_prior_state = True for Git.
+        Real use case for now - adjusting rootdir to point to worktree dir if dont_disrupt_prior_state = True for Git.
         """
         # Setup phase:
         original_rootdir = self.rootdir
