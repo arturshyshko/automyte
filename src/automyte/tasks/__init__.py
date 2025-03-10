811e0b89e31612461046efeb577ac027d79b4063
Artur Shyshko <arturshyshko@gmail.com>
2025-03-11 10:35:47 +1100

    Add fs.flush() util task

--- src/automyte/tasks/__init__.py
+++ src/automyte/tasks/__init__.py
@@ -1,10 +1,11 @@
 from . import vcs
 from .flow_control import conditionals, guards
-from .utils import Breakpoint
+from .utils import Breakpoint, fs
 
 __all__ = [
     "Breakpoint",
     "conditionals",
     "guards",
+    "fs",
     "vcs",
 ]
