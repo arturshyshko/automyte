c54b65b61bd21fa1894f7d440679fa704f7c0d08
Artur Shyshko <arturshyshko@gmail.com>
2025-03-11 15:29:09 +1100

    Add file_filtering demo example test

--- tests/examples/test_simplest_setup_01.py
+++ tests/examples/test_simplest_setup_01.py
@@ -17,10 +17,11 @@ if some_var:
 """
 
 
-def test_simplest_setup(tmp_git_repo):
-    def remove_redundant_is_true_comparisons(ctx: RunContext, file: File):
-        file.edit(re.sub(r"if (\w+) is True:", r"if \1:", file.get_contents()))
+def remove_redundant_is_true_comparisons(ctx: RunContext, file: File):
+    file.edit(re.sub(r"if (\w+) is True:", r"if \1:", file.get_contents()))
 
+
+def test_simplest_setup(tmp_git_repo):
     directory = tmp_git_repo({"src": {"some_python_file.py": file_contents}})
 
     Automaton(
