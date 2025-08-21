82dcb665755f5718878c0e98203baabde899d14b
Sarulatha Shanmugam <sarulathas@sahaj.ai>
2025-08-21 11:58:19 +1000

    Refactoring config methods into metdata parser and builders

--- src/automyte/config/builders/env.py
+++ src/automyte/config/builders/env.py
@@ -1,9 +1,8 @@
 from dataclasses import Field, dataclass
 import os
+from automyte.config.builders.metadata_parser import ConfigMetadataParser
 from automyte.config.fields import ConfigParams
 import typing as t
-
-from automyte.utils.config import get_fields_to_process, get_typed_value
 
 
 @dataclass
@@ -12,7 +11,7 @@ class EnvVarsConfigMixin:
     def parse_env_vars(cls) -> ConfigParams:
         env_config = ConfigParams()
         # get fields configurable via env_var including nested fields for vcs
-        fields_to_process: t.Sequence[Field] = get_fields_to_process(cls, "env_var")
+        fields_to_process: t.Sequence[Field] = ConfigMetadataParser.get_fields_to_process(cls, "env_var")
 
         for item in fields_to_process:
             param = item.metadata.get("env_var", None)
@@ -20,7 +19,7 @@ class EnvVarsConfigMixin:
             field_of = item.metadata.get("field_of", "config")
             field_name = item.metadata.get("name", item.name)
 
-            value = get_typed_value(kind, os.getenv(param)) if param else None
+            value = cls.get_typed_value(kind, os.getenv(param)) if param else None
 
             # explicit checking for None as value can be False
             if param and value is not None:
@@ -32,3 +31,15 @@ class EnvVarsConfigMixin:
                     env_config[field_of][field_name] = value
 
         return env_config
+
+    @classmethod
+    def get_typed_value(cls, kind: t.Any, value: str | None) -> str | bool | int | None:
+        if value is None:
+            return None
+
+        if kind is bool:
+            return value.strip().lower() == "true"
+        elif kind is int:
+            return int(value)
+
+        return value
