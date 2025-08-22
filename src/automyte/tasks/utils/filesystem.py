af2f02851ab629309bb7f3b6cf100f13929e6693
Sarulatha Shanmugam <sarulathas@sahaj.ai>
2025-08-22 14:58:41 +1000

    Adding feature to create new file in tasks
    * Adding feature to create new file in tasks
    
    * Moving file creation to Project explorer and using relative path

--- src/automyte/tasks/utils/filesystem.py
+++ src/automyte/tasks/utils/filesystem.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 from automyte.automaton import RunContext
+from automyte.automaton.types import TaskReturn
 from automyte.discovery import File
+from automyte.discovery.file.os_file import OSFile
 
 
 class flush:
@@ -13,3 +16,30 @@ class flush:
             file.flush()
         else:
             ctx.project.apply_changes()
+
+
+class create:
+    """Util to create a new file.
+    Input:
+        path: path relative to the project rootdir for the new file to be created
+        content: string content to be written into the file
+    """
+
+    def __init__(self, path: Path | str, content: str = ""):
+        self.path = Path(path) if isinstance(path, str) else path
+        self.content = content
+
+    def __call__(self, ctx: RunContext, file: File | None) -> TaskReturn:
+        try:
+            self.path = ctx.project.rootdir / self.path
+            file = ctx.project.explorer.add_file(path=self.path, content=self.content)
+            file.flush()
+
+        except Exception as e:
+            return TaskReturn(status="errored", instruction="abort", value=str(e))
+
+        return TaskReturn(
+            status="processed",
+            instruction="continue",
+            value=file,
+        )
--- tests/tasks/utils/test_filesystem.py
+++ tests/tasks/utils/test_filesystem.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 from automyte.automaton import RunContext
 from automyte.discovery import OSFile
+from automyte.tasks import vcs
 from automyte.tasks.utils import fs
+from automyte.utils import bash
 
 
 class TestFileSystemFlush:
@@ -12,3 +15,31 @@ class TestFileSystemFlush:
 
         with open(file.fullpath, "r") as disk_file:
             assert disk_file.read() == "test flushing"
+
+
+class TestFileSystemCreate:
+    def test_should_create_new_file_when_path_is_sent_as_string(self, run_ctx, tmp_local_project):
+        dir = tmp_local_project(structure={})
+        ctx: RunContext = run_ctx(dir)
+
+        result = fs.create("test/file.txt", "File Content")(ctx, None)
+
+        assert result.status == "processed"
+        assert (Path(dir) / "test/file.txt").exists()
+
+        file: OSFile = result.value
+        with open(file.fullpath, "r") as f:
+            assert f.read() == "File Content"
+
+    def test_should_create_new_file_when_path_is_sent(self, run_ctx, tmp_local_project):
+        dir = tmp_local_project(structure={})
+        ctx: RunContext = run_ctx(dir)
+
+        result = fs.create(Path("test/file.txt"), "File Content")(ctx, None)
+
+        assert result.status == "processed"
+        assert (Path(dir) / "test/file.txt").exists()
+
+        file: OSFile = result.value
+        with open(file.fullpath, "r") as f:
+            assert f.read() == "File Content"
